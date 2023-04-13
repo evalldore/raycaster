2eca92f94a9230034d5a7fbdef8506fd7297269b
evalldore <evalldore@gmail.com>
2023-04-12 22:01:09 -0400

    plane stuff

--- src/map.c
+++ src/map.c
@@ -67,27 +67,28 @@ static void	Draw_Floor()
 
 void Map_Draw(float x, float y, float a)
 {
-	float ra;
-	int r;
-	int	lineWidth = 8;
-	int numLines = WIDTH / lineWidth;
-	float planeX = 0, planeY = 0.66;
-
-	//Draw_Map(x, y, a);
-
 	Draw_Ceiling();
 	Draw_Floor();
-	ra = rotate(a, -DR * 30);
-	for(r = 0; r < 60; r++)
+	float rayAngle = rotate(a, -DR * 64);
+	float planeAngle = rotate(a, DR * 90);
+	fvec_t planeDir = {cos(planeAngle), sin(planeAngle)};
+	fvec_t dir = {cos(a), sin(a)};
+	glLineWidth(4.f);
+	Renderer_SetColor(1.0f, 1.0f, 1.0f);
+	Renderer_DrawLine(x + (planeDir.x * -30.f), y + (planeDir.y * -30.f), x + (planeDir.x * 30.f), y + (planeDir.y * 30.f));
+	int ray;
+	for(ray = 0; ray < 128; ray++)
 	{
-		float cameraX = 2.0f * r / 60.0f - 1.0f;
-		fvec_t rayDir = {cos(ra), sin(ra)};
+		float cameraX = 2.0f * (float)(ray / 128) - 1.0f;
+		//float rayDirX = dir.x + planeX * cameraX;
+    	//float rayDirY = dir.y + planeY * cameraX;
+		fvec_t rayDir = {cos(rayAngle), sin(rayAngle)};
 		ivec_t mapCheck = {(int)x, (int)y};
 		fvec_t rayStep = {
 			(rayDir.x == 0) ? INFINITY : fabs(1.0f / rayDir.x),
 			(rayDir.y == 0) ? INFINITY : fabs(1.0f / rayDir.y),
 		};
-		fvec_t vRayLength1D = {
+		fvec_t rayLength = {
 			(((float)mapCheck.x + 1) - x) * rayStep.x,
 			(((float)mapCheck.y + 1) - y) * rayStep.y
 		};
@@ -95,30 +96,30 @@ void Map_Draw(float x, float y, float a)
 		if (rayDir.x < 0) 
 		{
 			vStep.x = -1;
-			vRayLength1D.x = (x - (float)mapCheck.x) * rayStep.x;
+			rayLength.x = (x - (float)mapCheck.x) * rayStep.x;
 		}
 		if (rayDir.y < 0) 
 		{
 			vStep.y = -1;
-			vRayLength1D.y = (y - (float)mapCheck.y) * rayStep.y;
+			rayLength.y = (y - (float)mapCheck.y) * rayStep.y;
 		}
 		bool bTileFound = false;
-		float fDistance = 0.0f;
+		float distance = 0.0f;
 		int	dof = 0;
 		while (!bTileFound && dof <= 16)
 		{
-			if (vRayLength1D.x < vRayLength1D.y) 
+			if (rayLength.x < rayLength.y) 
 			{
 				mapCheck.x += vStep.x;
-				fDistance = vRayLength1D.x;
-				vRayLength1D.x += rayStep.x;
+				distance = rayLength.x;
+				rayLength.x += rayStep.x;
 				Renderer_SetColor(1.0f, 0.0f, 0.0f);
 			} 
 			else 
 			{
 				mapCheck.y += vStep.y;
-				fDistance = vRayLength1D.y;
-				vRayLength1D.y += rayStep.y;
+				distance = rayLength.y;
+				rayLength.y += rayStep.y;
 				Renderer_SetColor(0.7f, 0.0f, 0.0f);
 			}
 			if (mapCheck.x >= 0 && mapCheck.x < map_w && mapCheck.y >= 0 && mapCheck.y < map_h)
@@ -127,12 +128,12 @@ void Map_Draw(float x, float y, float a)
 		}
 		if (bTileFound)
 		{
-			fDistance *= cos(ra - a);
-			float lineH = fmin((TILE_SIZE * HEIGHT) / (fDistance * TILE_SIZE), HEIGHT);
+			distance *= cos(rayAngle - a);
+			float lineH = fmin((TILE_SIZE * HEIGHT) / (distance * TILE_SIZE), HEIGHT);
 			float lineO = (HEIGHT >> 1) - lineH / 2;
 			glLineWidth(8);
-			Renderer_DrawLine(240 + 4 + r * 8, lineO, 240 + 4 + r * 8, lineH + lineO);
+			Renderer_DrawLine(4 + ray * 8, lineO, 4 + ray * 8, lineH + lineO);
 		}
-		ra = rotate(ra, DR);
+		rayAngle = rotate(rayAngle, DR);
 	}
 }
